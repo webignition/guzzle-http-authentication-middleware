175177b43d38eb3e12f5ef2f9765794831a65896
Jon Cram <jon@webignition.net>
2018-12-18 18:21:05 +0000

    Add type constants

---
+++ src/HttpAuthenticationType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace webignition\Guzzle\Middleware\HttpAuthentication;
+
+class HttpAuthenticationType
+{
+    const BASIC = 'Basic';
+    const BEARER = 'Bearer';
+    const DIGEST = 'Digest';
+    const HOBA = 'HOBA';
+    const MUTUAL = 'Mutual';
+    const NEGOTIATE = 'Negotiate';
+    const OAUTH = 'OAuth';
+    const SCRAM_SHA_1 = 'SCRAM-SHA-1';
+    const SCRAM_SHA_256 = 'SCRAM-SHA-256';
+    const VAPID = 'vapid';
+}
